f3db94e46b8e5cdb1cfb0109b290bacf9ea80d44
bryanmwangindungu <bryanmwangi52@gmail.com>
2025-07-20 14:40:20 +0300

    7th commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1>User Info</h1>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
